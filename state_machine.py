eda9401abeceb309eb382a09430a3abcdd5f76cc
Park Jinsung <pjswin7@tukorea.ac.kr>
2025-10-30 20:22:02 +0900

    state_machine에 rules를 추가해 캐릭터 이동 규칙 만들기(초안)

--- state_machine.py
+++ state_machine.py
@@ -2,6 +2,21 @@
 class StateMachine:
     def __init__(self, start_state):
         self.cur_state = start_state
+        self.rules = rules
+
+    def handle_state_event(self, state_event):
+        # 첫 번째 방식(직접 인덱싱) 사용: 현재 상태의 전이 줄만 검사
+        for check_event in self.rules[self.cur_state].keys():
+            if check_event(state_event):
+                next_state = self.rules[self.cur_state][check_event]
+                prev_state = self.cur_state
+                prev_state.exit(state_event)
+                self.cur_state = next_state
+                self.cur_state.enter(state_event)
+                print(f'{prev_state.__class__.__name__} -> {self.cur_state.__class__.__name__}')
+                return
+        # 처리 못했을 때 메시지(선택)
+        # print('처리되지 않은 이벤트')
 
     def update(self):
         # 매 프레임 상태의 로직 실행
