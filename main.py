b3589b5335281a037641614bfa802a31710d319c
Park Jinsung <pjswin7@tukorea.ac.kr>
2025-11-18 21:37:20 +0900

    충돌 처리로 grass와 캐릭터들 간의 충돌 획인

--- main.py
+++ main.py
@@ -6,6 +6,48 @@ import time
 import game_framework
 
 
+def resolve_ground(obj, ground):
+    ol, ob, or_, ot = obj.get_bb()
+    gl, gb, gr, gt = ground.get_bb()
+
+
+    if or_ < gl or gr < ol:
+        return
+
+
+    if ob < gt:
+        dy = gt - ob
+        obj.y += dy
+        if hasattr(obj, 'vy'):
+            obj.vy = 0.0
+
+        if hasattr(obj, 'state_machine') and hasattr(obj, 'FALL'):
+            cur = obj.state_machine.cur_state
+
+            if cur == obj.FALL:
+                if obj.dir == 0:
+                    obj.state_machine.handle_state_event(('LANDED_IDLE', None))
+                else:
+                    obj.state_machine.handle_state_event(('LANDED_RUN', None))
+
+
+def resolve_body_block(a, b):
+    al, ab, ar, at = a.get_bb()
+    bl, bb, br, bt = b.get_bb()
+
+    # 둘이 안 겹치면 아무것도 하지 않음
+    if ar <= bl or br <= al:
+        return
+    if at <= bb or bt <= ab:
+        return
+
+
+    if a.x < b.x:
+        shift = bl - ar
+    else:
+        shift = br - al
+    a.x += shift
+
 
 open_canvas()
 
@@ -39,6 +81,9 @@ while running:
     boy.update()
     evil.update()
 
+    resolve_ground(boy, grass)
+    resolve_ground(evil, grass)
+    resolve_body_block(boy, evil)
 
     clear_canvas()
     grass.draw()
