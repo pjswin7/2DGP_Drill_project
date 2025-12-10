1ece216232de9fd6f7b3e0064575cb3c6923e9b8
Park Jinsung <pjswin7@tukorea.ac.kr>
2025-12-10 22:03:57 +0900

    2021180013 과제 완료

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import game_framework
 import title_mode as start_mode  # 시작은 타이틀 모드부터.
 
 open_canvas()
-bgm = load_music('cave/Dance_of_curse.mp3')
+bgm = load_music('sound/Dance_of_curse.mp3')
 bgm.set_volume(64)
 bgm.repeat_play()
 game_framework.run(start_mode)
